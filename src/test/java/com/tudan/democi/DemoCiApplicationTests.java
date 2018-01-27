d8f5ba847a21a72d273c0ecdf1dc6ca2709cf74d
Mario Tuđan <mtudjan@foi.hr>
2018-01-27 19:59:50 +0100

    Project setup

---
+++ src/test/java/com/tudan/democi/DemoCiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tudan.democi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
